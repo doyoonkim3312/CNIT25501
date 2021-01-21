1708255785afee9d06f3e628413ce36262e03b6c
Doyoon Kim <kim3312@purdue.edu>
2021-01-21 09:48:56 -0500

    Lab 0 - Getting Started (Check-off completed on Jan 21st, 2021)

---
+++ src/Lab0.java
@@ -0,0 +1,18 @@
+/**
+ * @author Doyoon Kim (kim3312@purdue.edu / doyoon3312@kakao.com)
+ */
+
+public class Lab0 {
+    public static void main(String[] args) {
+      String name = "Doyoon Kim";
+      String major = "Computer and Information Technology";
+      String year = "Sophomore";
+      String favoriteLanguage = "Java and Java related language (like Kotlin)";
+
+      System.out.println("Hello World! My name is " + name + "\n" +
+                "My major is " + major + "and I'm " + year + "\n" +
+              "My favorite programming language is " + favoriteLanguage);
+
+      System.exit(0);
+    }
+}
