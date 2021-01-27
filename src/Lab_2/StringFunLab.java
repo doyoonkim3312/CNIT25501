d22a831bc186e852da47ccaa3650b5b6ddb08559
Doyoon Kim <kim3312@purdue.edu>
2021-01-26 23:40:26 -0500

    Lab 1 - StringFunLab
    * [REFACTOR] Modify searchText's return so that it matches with lab manual
    * [FEAT] Add removeText that accept String and charSet and return new String that charSet is eliminated.

--- src/Lab_2/StringFunLab.java
+++ src/Lab_2/StringFunLab.java
@@ -11,9 +11,10 @@ public class StringFunLab {
         System.out.println(searchText("c", "abc def"));
     }
 
-    public static String searchText(String targetChar, String targetString) {
+    public static double searchText(String targetChar, String targetString) {
         String[] stringInArray = targetString.split("");
         int placeCounter = 0;
+        boolean isCharacterFound = false;
 
         for (int i = 0; i < stringInArray.length; i++) {
             if (stringInArray[i].compareTo(targetChar) == 0) {
@@ -22,6 +23,56 @@ public class StringFunLab {
                 placeCounter++;
             }
         }
-        return targetString + " " +placeCounter;
+
+        if (isCharacterFound) {
+            return placeCounter;
+        } else {
+            return -1;
+        }
+    }
+
+    public static String removeText(String targetCharSet, String targetString) {
+        String[] charSet = targetCharSet.split("");
+        String[] tarString = targetString.split("");
+        String result = "";
+
+        for (int i = 0; i < charSet.length; i++) {
+            for (int j = 0; j < tarString.length; j++) {
+                if (charSet[i].compareTo(tarString[j]) == 0) {
+                    continue;
+                } else {
+                    result += tarString[j];
+                }
+            }
+        }
+        return result;
+    }
+
+    public static String addText(int position, String inputCharSet, String targetString) {
+        String[] charSet = inputCharSet.split("");
+        String[] tarString = targetString.split("");
+        String result = "";
+
+        return result;
+    }
+
+    public static String reverseText(String targetString) {
+        return "";
+    }
+
+    public static String reverseEachWord(String targetString) {
+        return "";
+    }
+
+    public static void printString(String targetString) {
+        System.out.println(targetString);
+    }
+
+    public static void enterNewString(String newString) {
+
+    }
+
+    public static void quit() {
+        System.exit(100);
     }
 }
