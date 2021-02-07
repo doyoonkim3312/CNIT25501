da2d728b28771ccf27a02315b550e68907301f46
Doyoon Kim <kim3312@purdue.edu>
2021-02-07 00:37:19 -0500

    Lab3 - Web Chatting Client program
    * [STYLE] Add Javadoc

--- src/Lab_3/Enums.java
+++ src/Lab_3/Enums.java
@@ -5,5 +5,6 @@ enum UserType {CLIENT, MANAGER, GUEST};
 /**
  * Value for sender validation.
  * LOG ON USER ALWAYS ASSIGNED TO USER 1
+ * This enum class should be modified later in order to corresponding chatroom that has more than 2 users.
  */
 enum SenderRotation {USER1, USER2, NULL}
