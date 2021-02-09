fef5ba0a183b350efd9dfaaf6346b634cb0c7817
Doyoon Kim <kim3312@purdue.edu>
2021-02-08 20:59:56 -0500

    Lab3 - Web Chatting Client program
    * [FEAT] Addd UserDB class that performs a role of DB to store users created.
    
    Method addItems, removeItems are created. Each method are overloaded.

---
+++ src/Lab_3/UserDB.java
@@ -0,0 +1,34 @@
+package Lab_3;
+
+import java.util.ArrayList;
+
+public class UserDB {
+    private ArrayList<User> UserList = new ArrayList<>();
+
+    public void addItems(Client client) {
+        User item = new User(client);
+        UserList.add(item);
+    }
+
+    public void addItems(Manager manager) {
+        User item = new User(manager);
+        UserList.add(item);
+    }
+
+    public void removeItems(Client client) {
+        for (int i = 0; i < UserList.size(); i++) {
+            if (client.getUserUniqueId() == UserList.get(i).getUserUniqueId()) {
+                UserList.remove(i);
+            }
+        }
+    }
+
+    public void removeItems(Manager manager) {
+        for (int i = 0; i < UserList.size(); i++) {
+            if (manager.getUserUniqueId() == UserList.get(i).getUserUniqueId()) {
+                UserList.remove(i);
+            }
+        }
+    }
+
+}
