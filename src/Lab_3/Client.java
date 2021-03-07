2687e36f44b970258176cba751c9b4dff0c01a46
Doyoon Kim <kim3312@purdue.edu>
2021-03-06 22:42:31 -0500

    Lab3 - Chatting Client
    * [REFACTOR] createNewClient method is relocated to this class.

--- src/Lab_3/Client.java
+++ src/Lab_3/Client.java
@@ -1,12 +1,14 @@
 package Lab_3;
 
+import java.util.Scanner;
+
 public class Client  extends User {
     private UserType userType;
 
     // Default constructor for guest user.
     public Client() {
-        super("Guest", "", "guest", "0000");
-        userType = UserType.GUEST;
+        super("NaU", "", "NaU", "0000");
+        userType = UserType.NULL;
     }
 
     // Recommended Constructor
@@ -31,6 +33,27 @@ public class Client  extends User {
 
     public UserType getUserType() {
         return userType;
+    }
+
+    public static Client createNewClient(Scanner inputModel, UserDB userDB) {
+        System.out.println("Enter the First Name");
+        String firstName = inputModel.nextLine();
+
+        System.out.println("Enter the Last Name");
+        String lastName = inputModel.nextLine();
+
+        System.out.println("Enter the Nick Name");
+        String nickName = inputModel.nextLine();
+        System.out.println(userDB.nickNameValidation(nickName));
+        while(userDB.nickNameValidation(nickName)) {
+            System.out.println("Nickname is already used. Enter new nickname");
+            nickName = inputModel.nextLine();
+        }
+
+        System.out.println("Enter the password");
+        String passwd = inputModel.nextLine();
+
+        return new Client(firstName, lastName, nickName, passwd, UserType.CLIENT);
     }
 
 }
