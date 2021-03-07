46eadae7bd5139a415dd3a9aca0bac0f6ee6ae10
Doyoon Kim <kim3312@purdue.edu>
2021-03-07 03:46:31 -0500

    Lab 4 - Simple Calculator
    * [FEAT] Swing LinearLayout Class. Takes multiple JComponent and return JPanel with either horizontal or vertical layout.

---
+++ src/Lab_4/LinearLayoutPanel.java
@@ -0,0 +1,55 @@
+package Lab_4;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class LinearLayoutPanel {
+    private JPanel panel = new JPanel();
+    private Orient orient;
+    private ArrayList<JComponent> components = new ArrayList<JComponent>();
+    private ArrayList<GridBagConstraints> contrainsts = new ArrayList<GridBagConstraints>();
+
+    public LinearLayoutPanel(Orient orient) {
+        this.orient = orient;
+        panel.setLayout(new GridBagLayout());
+    }
+
+    public void add(JComponent component) {
+        components.add(component);
+        contrainsts.add(new GridBagConstraints());
+        notifyConstraints();
+    }
+
+    public void add(JComponent...components) {
+        for (JComponent component: components) {
+            this.components.add(component);
+            contrainsts.add(new GridBagConstraints());
+        }
+    }
+
+    public void notifyConstraints() {
+        switch (orient) {
+            case HORIZONTAL -> {
+                for(int i = 0; i < contrainsts.size(); i++) {
+                    contrainsts.get(i).gridy = i;
+                }
+            }
+            case VERTICAL -> {
+                for (int i = 0; i < contrainsts.size(); i++) {
+                    contrainsts.get(i).gridx = i;
+                }
+            }
+        }
+    }
+
+    public JPanel inflate() {
+        for (JComponent component: components) {
+            for (GridBagConstraints constraint: contrainsts) {
+                panel.add(component, constraint);
+            }
+        }
+        return panel;
+    }
+}
+
