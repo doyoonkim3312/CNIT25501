f42105292fdb452c2f1a1e1f78d2fd2e0a42f699
Doyoon Kim <kim3312@purdue.edu>
2021-03-11 11:09:19 -0500

    Commit [031121] Lab 4 - Simple Calculator
    * [FEAT] Add new enum values (Operator) for operator button control. (CLEAR, SIGN, PERCENTAGE, SQRT, POW

--- src/Lab_4/Enums.java
+++ src/Lab_4/Enums.java
@@ -2,4 +2,4 @@ package Lab_4;
 
 enum Orient { HORIZONTAL, VERTICAL }
 
-enum Operator { PLUS, MINUS, MULTIPLY, DIVIDE, NULL }
+enum Operator { PLUS, MINUS, MULTIPLY, DIVIDE, CLEAR, SIGN, PERCENTAGE, SQRT, POW, NULL }
