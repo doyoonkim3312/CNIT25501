51b3f680a0809ab0da424c6abcbdc8e22a5c5ad4
Doyoon Kim <kim3312@purdue.edu>
2021-03-15 23:33:03 -0400

    Lab 4 - Simple One-line Calculator
    * [FEAT] add calculation controls for basic calculation (Plus, Minus, Multiply, Divide)
    
    * [FEAT] add equal method that calculates final result and prompt on label
    
    * [FEAT] add method valueParser() for retrieving string value from label and covert value to double value.

--- src/Lab_4/Calculation.java
+++ src/Lab_4/Calculation.java
@@ -1,30 +1,76 @@
 package Lab_4;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 
 public class Calculation {
     private JLabel mainLabel;
-    private Operator operator;
+    private Operator operatorClicked, operatorAssigned;
     private double numberStored;
 
     public Calculation(JLabel label) {
         mainLabel = label;
-        operator = Operator.NULL;
+        operatorAssigned = Operator.NULL;
+        operatorClicked = Operator.NULL;
         // assignedOperator = Operator.NULL;
         numberStored = 0;
     }
 
-    public void addition(double number) {
-        basicCalculation(Double.parseDouble(mainLabel.getText()));
+    public void addition() {
+        operatorClicked = Operator.PLUS;
+        basicCalculation(valueParser());
         mainLabel.setText(Double.toString(numberStored));
     }
 
+    public void minus() {
+        operatorClicked = Operator.MINUS;
+        basicCalculation(valueParser());
+        mainLabel.setText(Double.toString(numberStored));
+    }
+
+    public void multiply() {
+        operatorClicked = Operator.MULTIPLY;
+        basicCalculation(valueParser());
+        mainLabel.setText(Double.toString(numberStored));
+    }
+
+    public void divide() {
+        operatorClicked = Operator.DIVIDE;
+        basicCalculation(valueParser());
+        mainLabel.setText(Double.toString(numberStored));
+    }
+
+    public void equal() {
+        basicCalculation(valueParser());
+        mainLabel.setText(Double.toString(numberStored));
+    }
+
+    public void clear() {
+        if (mainLabel.getText().equals("0.0")) {
+            numberStored = 0;
+            operatorClicked = Operator.NULL;
+            operatorAssigned = Operator.NULL;
+        } else {
+            mainLabel.setText("0.0");
+        }
+    }
+
     public void basicCalculation(double input) {
-        switch (operator) {
+        switch (operatorAssigned) {
             case PLUS -> numberStored += input;
             case MINUS -> numberStored -= input;
             case DIVIDE -> numberStored /= input;
             case MULTIPLY -> numberStored *= input;
+            default -> numberStored = input;
+        }
+        operatorAssigned = operatorClicked;
+    }
+
+    private double valueParser() {
+        if (!mainLabel.getText().equals("")) {
+            return Double.parseDouble(mainLabel.getText());
+        } else {
+            return 0.0;
         }
     }
 
