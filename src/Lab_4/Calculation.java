ae7fe237f45321c72cf515dc8933780d47fe8c39
Doyoon Kim <kim3312@purdue.edu>
2021-03-16 20:33:02 -0400

    Lab 4 - Simple Calculator
    * [FEAT] Add remain operator controls (sign, percentage, e, pi, pow, sqRoot)
    
    * [REFACTOR] Modify clear operator control. Now, when clear button is clicked, 0 is prompted.

--- src/Lab_4/Calculation.java
+++ src/Lab_4/Calculation.java
@@ -2,6 +2,7 @@ package Lab_4;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
+import java.math.MathContext;
 
 public class Calculation {
     private JLabel mainLabel;
@@ -40,18 +41,63 @@ public class Calculation {
         mainLabel.setText(Double.toString(numberStored));
     }
 
+    /**
+     * <h1>sqRoot</h1>
+     * <p>This method calculates squared root of input value, using Babylonian method.</p>
+     * <p>A sequence's term is calculated until its 10th term. Calculate lim is way more precise, but</p>
+     * <p>since this is a simple calculator, 10th term of sequence ensures enough decimal value.</p>
+     * @return squared root of num
+     */
+    public double sqRoot() {
+        double num = valueParser();
+        double base = 0;
+        while (base < num) {
+            base++;
+            if ((base * base) == num) {
+                return base;
+            }
+        }
+
+        double termInSequence = ((base * base) + num) / (2 * base);
+        for (int i = 0; i < 10; i++) {
+            termInSequence = ((termInSequence * termInSequence) + num) / (2 * termInSequence);
+        }
+        return Double.parseDouble(String.format("%.9f", termInSequence));
+    }
+
+    public void pow() {
+        mainLabel.setText(Double.toString(StrictMath.pow(valueParser(), 2)));
+    }
+
+    public void pi() {
+        mainLabel.setText(Double.toString(StrictMath.PI));
+    }
+
+    public void e() {
+        mainLabel.setText(Double.toString(StrictMath.E));
+    }
+
     public void equal() {
         basicCalculation(valueParser());
         mainLabel.setText(Double.toString(numberStored));
+        operatorAssigned = Operator.NULL;
+    }
+
+    public void percentage() {
+        mainLabel.setText(Double.toString(0.01 * valueParser()));
+    }
+
+    public void sign() {
+        mainLabel.setText(Double.toString(-1 * valueParser()));
     }
 
     public void clear() {
-        if (mainLabel.getText().equals("0.0")) {
+        if (mainLabel.getText().equals("0")) {
             numberStored = 0;
             operatorClicked = Operator.NULL;
             operatorAssigned = Operator.NULL;
         } else {
-            mainLabel.setText("0.0");
+            mainLabel.setText("0");
         }
     }
 
